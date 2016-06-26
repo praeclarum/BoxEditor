a22074f5d959d6c09b06971ef1a2bf3b71dbfb8c
Frank A. Krueger <fak@praeclarum.org>
2016-06-25 22:55:19 -0700

    Prevent overlaps

--- BoxEditor/Diagram.cs
+++ BoxEditor/Diagram.cs
@@ -113,7 +113,116 @@ namespace BoxEditor
 				d = d.UpdateBox(b, nb);
 				newBs.Add(nb);
 			}
-			return Tuple.Create(d, guides, newBs.ToImmutableArray());
+			var newBsI = newBs.ToImmutableArray();
+
+			d = d.PreventOverlaps(newBsI, offset);
+
+			return Tuple.Create(d, guides, newBsI);
+		}
+
+		public Diagram PreventOverlaps(ImmutableArray<Box> staticBoxes, Point staticOffset)
+		{
+			var d = this;
+			var n = d.Boxes.Length;
+
+			var iterChanged = true;
+			var maxIters = 100;
+
+			var offsets = d.Boxes.Select(x => Point.Zero).ToList();
+
+			for (var iter = 0; iter < maxIters && iterChanged; iter++)
+			{
+				iterChanged = false;
+				for (var i = 0; i < n; i++)
+				{
+					var a = d.Boxes[i];
+
+					for (var j = i + 1; j < n; j++)
+					{
+						var b = d.Boxes[j];
+
+						var overlap = GetMarginOverlap(a, offsets[i], b, offsets[j], staticOffset);
+
+						if (Math.Abs(overlap.X) < 1e-5 && Math.Abs(overlap.Y) < 1e-5) continue;
+
+						if (staticBoxes.Contains(a))
+						{
+							if (staticBoxes.Contains(b))
+							{
+								// Nothing
+							}
+							else
+							{
+								iterChanged = true;
+								var dx = a.Frame.Center.X < b.Frame.Center.X ? 1 : -1;
+								var dy = a.Frame.Center.Y < b.Frame.Center.Y ? 1 : -1;
+								offsets[j] += new Point(dx * overlap.X, dy * overlap.Y);
+							}
+						}
+						else {
+							if (staticBoxes.Contains(b))
+							{
+								iterChanged = true;
+								var dx = a.Frame.Center.X < b.Frame.Center.X ? -1 : 1;
+								var dy = a.Frame.Center.Y < b.Frame.Center.Y ? -1 : 1;
+								offsets[i] += new Point(dx * overlap.X, dy * overlap.Y);
+							}
+							else
+							{
+								iterChanged = true;
+								var dx = a.Frame.Center.X < b.Frame.Center.X ? -1 : 1;
+								var dy = a.Frame.Center.Y < b.Frame.Center.Y ? -1 : 1;
+								offsets[i] += new Point(dx * overlap.X / 2, dy * overlap.Y / 2);
+								offsets[j] -= new Point(dx * overlap.X / 2, dy * overlap.Y / 2);
+							}
+						}
+
+						//Debug.WriteLine($"I={iter}, OVERLAP {overlap}, C={iterChanged}");
+					}
+				}
+			}
+
+			for (var i = 0; i < n; i++)
+			{
+				var b = d.Boxes[i];
+				var o = offsets[i];
+				if (Math.Abs(o.X) > 1e-12 || Math.Abs(o.Y) > 1e-12)
+				{
+					d = d.UpdateBox(b, b.Move(o));
+				}
+			}
+
+			return d;
+		}
+
+		Point GetMarginOverlap(Box a, Point ao, Box b, Point bo, Point offset)
+		{
+			var maxMargin = new Size(Math.Max(a.Style.Margin.Width, b.Style.Margin.Width),
+									 Math.Max(a.Style.Margin.Height, b.Style.Margin.Height));
+			var amr = a.Frame.GetInflated(maxMargin / 2) + ao;
+			var bmr = b.Frame.GetInflated(maxMargin / 2) + bo;
+			if (amr.Intersects(bmr))
+			{
+				var dx1 = bmr.Right - amr.Left;
+				var dx2 = amr.Right - bmr.Left;
+				var dx = (Math.Abs(dx1) <= Math.Abs(dx2)) ? dx1 : dx2;
+				var dy1 = bmr.Bottom - amr.Top;
+				var dy2 = amr.Bottom - bmr.Top;
+				var dy = (Math.Abs(dy1) <= Math.Abs(dy2)) ? dy1 : dy2;
+				if (Math.Abs(dx) <= Math.Abs(dy))
+				{
+					dy = 0;
+				}
+				else
+				{
+					dx = 0;
+				};
+				//Debug.WriteLine($"{DateTime.Now} DX = {dx} AMR = {amr}");
+				return new Point(dx, dy);
+			}
+			else {
+				return Point.Zero;
+			}
 		}
 
 		public static Diagram Create(
